1fb4e84d49210740af662b05e9dcd84807771454
itszoeyhuang <itszoeyhuang@gmail.com>
2025-04-14 21:26:29 -0700

    reduce size

--- src/component/Line.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Line({ width = 1300, color="white"}){
-    return(
-        <svg width={width} height="1" viewBox="0 0 {width} 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <line y1="0.5" x2={width} y2="0.5" stroke={color}/>
-        </svg>
-    );
-}
